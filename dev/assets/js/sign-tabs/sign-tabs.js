752c2c4aed8897a2cbb539e875136f61ea84ae56
Pavel <pavliksedoy@gmail.com>
2020-03-02 18:49:14 +0200

    Sign tabs corectly

--- dev/assets/js/sign-tabs/sign-tabs.js
+++ dev/assets/js/sign-tabs/sign-tabs.js
@@ -1,11 +1,40 @@
 const signTabs = document.getElementById('signTabs')
+let signUpActive = false
+
+// console.log(signTabs.classList)
 
 // On sign up hover
 const handleRegTabHover = () => {
-  signTabs.classList.add('signup-active')
+  if (checkActiveTab()) signUpActive = true
+  else signTabs.classList.add('signup-active')
+}
+
+const handleLogTabHover = () => {
+  if (checkActiveTab()) {
+    signUpActive = true
+    signTabs.classList.remove('signup-active')
+  }
 }
 
 // On sign up out
 const handleRegTabOut = () => {
-  signTabs.classList.remove('signup-active')
+  if (signUpActive) return false
+  else signTabs.classList.remove('signup-active')
+}
+
+const handleLogTabOut = () => {
+  if (signUpActive) signTabs.classList.add('signup-active')
+  else signTabs.classList.remove('signup-active')
+}
+
+// Check active tab
+const checkActiveTab = () => {
+  const signClasses = signTabs.classList
+  let isSignUpActive = false
+
+  signClasses.forEach( val => {
+    isSignUpActive = val == 'signup-active' ? true : false
+  })
+
+  return isSignUpActive
 }
